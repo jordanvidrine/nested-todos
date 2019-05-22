425df7c3c0cf2ae98714fb554cc920a0091d41b1
Jordan Vidrine <Jordan@JordanMacBook.local>
2019-05-22 13:20:01 -0500

    rendering only with javascript--issues still arising

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-//working on rendering using javascript+jquery only
-//issue: when removing last item in todos, it returns undefined 
+//current issues:
+//nested ULs appear BEFORE the LI they belong too ie.
+// one -1
+// one -2
+//one
+// two-1
+// two-2
+//two
 
+//also, nesting onto a subTodo returns an error from findTodo
 
 var todoInput = document.getElementById("todo-input");
 $("#add-todo-btn").on('click',addTodo)
@@ -32,12 +39,15 @@ function addTodo(){
   $("#todo-input").focus()
 }
 
-function findTodo(e) {
-  debugger;
-  let targetId = e.target.parentElement.id;
-  let todoToAddSubTo = todos.filter(e => e.id == targetId)[0];
-  let todoIndex = todos.indexOf(todoToAddSubTo);
-  return todos[todoIndex]
+// function findTodo(e) {
+//   let targetId = e.target.parentElement.id;
+//   let todoToAddSubTo = todos.filter(e => e.id == targetId)[0];
+//   let todoIndex = todos.indexOf(todoToAddSubTo);
+//   return todos[todoIndex]
+//}
+
+function findTodo(e){
+
 }
 
 function addSubTodo(e){
@@ -80,8 +90,8 @@ function clearCompleted() {
 }
 
 function filterTodos(e){
-  console.log(e.target.value)
   let filteredTodos;
+  //create a new local array of todos based on radio button clicked
   if (e.target.value == "show-completed") {
     filteredTodos = todos.filter(e => e.completed == true);
   } else if (e.target.value == "show-all") {
@@ -90,6 +100,7 @@ function filterTodos(e){
   else {
     filteredTodos = todos.filter(e => e.completed == false);
   }
+  //render the filtered todos, but do not store it
   render(filteredTodos);
 }
 
@@ -103,20 +114,11 @@ function store(namespace, data) {
   }
 }
 
-// function render(todos) {
-//   $('#todos').html(template(todos));
-//   todoInput.value = '';
-//   renderFooter()
-// }
-
 function render(todos) {
-  if (!todos) {
-    $("#todos").html('');
-    return;
-  }
   if (todos.length == 0) {
     $("#todos").html('');
     todoInput.value = '';
+    return;
   }
 
   $("#todos").html(todosParser(todos));
@@ -126,10 +128,28 @@ function render(todos) {
 function todosParser(todos) {
   let html = '';
 
-  if (todos.length > 1) {
+  if (todos.length >= 1) {
     //recursively builds html to append to #todos UL
-    for (let i = 0; i < todos.length; i++) {
-      let todo = todos[i];
+    for (let a = 0; a < todos.length; a++) {
+      let todo = todos[a];
+      //builds up the inner arrays if subTodo exists
+      if (todo.subTodos.length > 0) {
+        for (let b = 0; b < todo.subTodos.length; b++) {
+          let subTodo = todo.subTodos[b];
+          //if this is the beginning of a subTodo array, add <ul> to the front
+          if (b == 0) {
+            html += '<ul>'
+          }
+          //if this is the end of a subTodo array, add </ul> to the back
+          if (b == todo.subTodos.length - 1) {
+            html += todosParser(subTodo) + '</ul>'
+          }
+          //this this isnt the beg or end, return an <li> from todoparser
+          else {
+            html += todosParser(subTodo)
+          }
+        }
+      }
       html += todosParser(todo);
     }
     return html;
@@ -140,10 +160,19 @@ function todosParser(todos) {
     let completeTodoButton = `<button id="complete-todo">`;
     if (todos.completed) {
       completeTodoButton += `(*)</button>`
-    } else {
+      //only inserts button IF subTodos are all complete or non-existant
+    } else if (todos.subTodos.filter(e=>e.completed).length === todos.subTodos.length) {
       completeTodoButton += `()</button>`
+    } else {
+      completeTodoButton = '';
     }
-    return `<li id="${todos.id}" ${completed}>${completeTodoButton} ${todos.title} <button id="delete-todo-btn">remove</button><button id="add-sub-todos-btn">add sub-todos</button></li>`
+    return (
+    `<li id="${todos.id}" ${completed}>` +
+      `${completeTodoButton} ${todos.title}` +
+      `<button id="delete-todo-btn">remove</button>` +
+      `<button id="add-sub-todos-btn">add sub-todos</button>` +
+    `</li>`
+    )
   }
 }
 
